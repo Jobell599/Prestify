019732a64213d1744a0b66341ca3a4b2d01a95fd
Jobell599 <jobeldicley123@gmail.com>
2024-10-24 15:09:36 -0400

    Practicing the routers

--- src/Prestify/Prestify.Web/Controllers/HomeController.cs
+++ src/Prestify/Prestify.Web/Controllers/HomeController.cs
@@ -6,11 +6,15 @@ namespace Prestify.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
+        }
+
+        public IActionResult AnAction()
+        {
+            return View();
         }
 
         public IActionResult Index()
